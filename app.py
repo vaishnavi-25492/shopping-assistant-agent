c9069bc5a44a1f2b35862dd21591efa663921a59
vaishnavi-25492 <24R21A0590@mlrit.ac.in>
2026-08-26 15:34:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("Shopping Assistant Agent")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    query = request.form.get("query")
+
+    return render_template(
+        "results.html",
+        query=query
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
